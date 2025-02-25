fc2ff34f18c96b85f937ce571aca1ee839939270
Fernando Lana <loslanas@gmail.com>
2025-02-25 11:13:44 -0300

    Correção do footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -9,7 +9,7 @@ function Footer() {
             <div className="flex justify-center bg-red-900 text-white">
                 <div className="container flex flex-col items-center py-4">
                     <p className='text-xl font-bold'>
-                            Fármacia | Copyright: {data}
+                            Farmácia | Copyright: {data}
                         </p>
                     <p className='text-lg'>Acesse nossas redes sociais</p>
                     <div className='flex gap-2'>
