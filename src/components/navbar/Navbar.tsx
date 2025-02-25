6fc6b42d806edc3bedc8fd1e55e18df7b2a77ddc
Fernando Lana <loslanas@gmail.com>
2025-02-25 09:33:00 -0300

    Criar Navbar, Footer e Home

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,54 @@
+
+import { Link } from "react-router-dom"
+
+function Navbar() {
+    return (
+        <>
+            <div className='w-full flex justify-center py-4
+            			   bg-red-900 text-white'>
+            
+                <div className="container flex justify-between text-lg">
+                
+                
+                <img 
+                            src="https://ik.imagekit.io/22g34n0mo/produtos_farmacia/logo.png?updatedAt=1740485357284" 
+                            alt="Imagem da Página Home" 
+                            width="200px"
+                        />
+               
+
+<div className="relative flex items-center justify-center w-2/5 text-black">
+						<form 
+							className="flex items-center justify-center w-full"
+						>
+							<input
+								className="w-10/12 px-4 py-4 bg-white rounded-lg h-9 focus:outline-none"
+								type="search"
+								placeholder="Pesquisar produto"
+								id="busca"
+								name="busca"
+								required
+							/>
+							<button
+								type="submit"
+								className="h-9 w-9 p-2.5 ms-2 text-sm font-medium text-white bg-teal-500 hover:bg-teal-900 rounded-lg border border-teal-700"
+							>
+	
+							</button>
+						</form>
+					</div>        
+
+                    <div className='flex gap-4'>
+                        Postagens
+                        Temas
+                        Cadastrar tema
+                        Perfil
+                        Sair
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
